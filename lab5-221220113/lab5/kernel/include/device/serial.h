212d83409fb55946c2b57f7a759df058965bcb88
Zrq436 <1448853658@qq.com>
2024-06-12 19:29:26 +0800

    final push

--- lab5-221220113/lab5/kernel/include/device/serial.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SERIAL_H__
-#define __SERIAL_H__
-
-void initSerial(void);
-void putChar(char);
-void putString(const char *);
-void putInt(int);
-
-#endif
--- lab5-221220113/lab5/utils/genFS/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TYPES_H__
-#define __TYPES_H__
-
-typedef unsigned int   uint32_t;
-typedef unsigned short uint16_t;
-typedef unsigned char  uint8_t;
-
-#endif
